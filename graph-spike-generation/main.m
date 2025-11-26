6374bb3dd106aae0d816573ed674f29a84c650b9
Didgety <jnbv00@gmail.com>
2025-11-25 18:26:17 -0800

    fix datetime

--- graph-spike-generation/main.m
+++ graph-spike-generation/main.m
@@ -1,22 +1,30 @@
 clear all; close all; clc;
 
-batchstamp = string(datetime('now'), "dd-MMM-yyyy_HH:mm:ss");
+batchstamp = string(datetime('now'), "dd-MMM-yyyy_HH-mm-ss");
 batchdir = fullfile('networks', batchstamp);
 mkdir(batchdir);
 
 %% parameters
-n_graphs = 1; % num graphs to make
-n = 50; % num neurons to generate
+n_graphs = 100; % num graphs to make
+n = 500 + randi([-100, 100]); % num neurons to generate
 r = 0.8; % proportion of neurons that are E
 Ne = floor(0.8*n); Ni = n-Ne;
 i_scaling = 3; % how much stronger than excitation we want inhibition to be
+
 % multifractal parameters
-% P = [0.8,0.5;0.5,0.4]; % default
-% P = [0.9 0.05 ; 0.05 0.9]; % strong
-P = [0.5, 0.5; 0.5, 0.5]; % weak
-L = [0.7,0.3];
+P = [
+    0.8,0.5;
+    0.5,0.4
+    ]; % p_ij(r) matrix
+L = [0.7,0.3]; % side lengths
 M = length(L); K = 3;
 isDirected = 1; isBinary = 0;
+
+%% Randomized parameter sets of the following forms:
+% K = 1:5, M = 2, p_ij(r) and l_i same
+% K = 4, M = 2, R = 3, 4 different sets of initial unit square model parameters
+% K = 5, M = 2, R = 3, l_i [(0.1, 0.9), (0.3, 0.7), (0.5, 0.5), (0.7, 0.3), (0.9, 0.1)], p_ij(r) same
+% R = 1:4
 
 %% generate n graph-spiking pairs
 n_zeros = ceil(log10(n_graphs));
