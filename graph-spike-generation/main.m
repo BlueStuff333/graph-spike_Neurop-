89bded823443a62b67def127197ce6a5eed5b38f
Didgety <jnbv00@gmail.com>
2025-11-27 00:13:19 -0800

    add train/test split

--- graph-spike-generation/main.m
+++ graph-spike-generation/main.m
@@ -11,6 +11,8 @@ end
 batchstamp = string(datetime('now'), "dd-MMM-yyyy_HH-mm-ss");
 batchdir = fullfile('networks', batchstamp);
 mkdir(batchdir);
+mkdir(fullfile('networks', batchstamp, 'train'));
+mkdir(fullfile('networks', batchstamp, 'test'));
 
 %% Randomized parameter sets of the following forms:
 % K = 1:5, M = 2, p_ij(r) and l_i same
@@ -18,15 +20,15 @@ mkdir(batchdir);
 % K = 5, M = 2, R = 3, l_i [(0.1, 0.9), (0.3, 0.7), (0.5, 0.5), (0.7, 0.3), (0.9, 0.1)], p_ij(r) same
 % R = 1:4
 %% generate n graph-spiking pairs
-n_graphs = 10; % num graphs to make
+n_graphs = 10^6; % num graphs to make
+train_test = 0.8;
 n_zeros = ceil(log10(n_graphs));
 adj_lists = cell(n_graphs, 1);
 firings_list = cell(n_graphs, 1);
 tic
 parfor i = 1:n_graphs
     % randomize parameters
-    % n = 500 + randi([-100, 100]); % num neurons to generate
-    n = 50; % TODO test value
+    n = 500 + randi([-100, 100]); % num neurons to generate
     r = 0.8; % proportion of neurons that are E
     Ne = floor(0.8*n); Ni = n-Ne;
     i_scaling = 3; % how much stronger than excitation we want inhibition to be
@@ -63,23 +65,29 @@ parfor i = 1:n_graphs
 
     % save the graph and its spikes in a .mat file
     filename = sprintf(['graph-spike_%0', num2str(n_zeros), 'd.mat'], i);
-    filepath = fullfile(batchdir, filename);
+    if( i <= n_graphs * train_test)
+        out_path = fullfile(batchdir, 'train', filename);
+    else
+        out_path = fullfile(batchdir, 'test', filename);
+    end
+    
     S = struct('adj', adj_lists{i}, 'e_locs', e_locs, 'i_locs', i_locs, 'firings', firings_list{i});
-    save(filepath, '-fromstruct', S);
+    save(out_path, '-fromstruct', S);
 
     %clc; 
     disp(num2str(i));
 end
 disp(sprintf('Graph spike gen time: %.2f', toc));
 
-for i = 1:n_graphs
-    % create & save jpg of the graph
-    plotname = sprintf('graph_%d', i);
-    plotsdir = fullfile(batchdir, 'plots'); mkdir (plotsdir);
-    node_colors = saveCliqueColors(adj_lists{i}, plotsdir, plotname);
-    % save the rasters in a jpg
-    raster_dir = fullfile(batchdir, 'rasters'); mkdir(raster_dir);
-    saveRasterPlots(firings_list{i}, node_colors, raster_dir, i);
-end
+% TODO enable/disable visualization at will
+% for i = 1:n_graphs
+%     % create & save jpg of the graph
+%     plotname = sprintf('graph_%d', i);
+%     plotsdir = fullfile(batchdir, 'plots'); mkdir (plotsdir);
+%     node_colors = saveCliqueColors(adj_lists{i}, plotsdir, plotname);
+%     % save the rasters in a jpg
+%     raster_dir = fullfile(batchdir, 'rasters'); mkdir(raster_dir);
+%     saveRasterPlots(firings_list{i}, node_colors, raster_dir, i);
+% end
 
 disp('complete');
