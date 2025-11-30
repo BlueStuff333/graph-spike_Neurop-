6655051d2191fb52e1a1de4a6f4ca7fef07ff453
Didgety <jnbv00@gmail.com>
2025-11-29 16:01:24 -0800

    increase batch size

--- Neurop-generation/train_inverse.py
+++ Neurop-generation/train_inverse.py
@@ -401,7 +401,7 @@ if __name__ == "__main__":
                         help='Also predict neuron positions')
     
     # Training parameters
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--weight_decay', type=float, default=1e-4)
